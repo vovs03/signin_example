26ccf6b065201b1de697f2477b503d7b96ccb493
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-08-17 20:46:11 +0300

    Add: 🦊 Step 2: Enable sign in progress tracking

--- lib/main.dart
+++ lib/main.dart
@@ -68,9 +68,30 @@ class _SignUpFormState extends State<SignUpForm> {
     Navigator.of(context).pushNamed('/welcome');
   }
 
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress, //NEW
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -112,7 +133,8 @@ class _SignUpFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed: 
+              _formProgress == 1 ? _showWelcomeScreen : null, //UPDATED
             child: const Text('Sign up'),
           ),
         ],
