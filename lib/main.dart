64a4a15388bffb449de77673d357a8c57f1bf7b1
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-08-17 17:39:37 +0300

    Add: v.0.1.0 - WelcomeScreen/ Step 1

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class SignUpApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => const SignUpScreen(),
+        '/welcome': (context) => const WelcomeScreen(),
       },
     );
   }
@@ -34,7 +35,20 @@ class SignUpScreen extends StatelessWidget {
   }
 }
 
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen();
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:
+          Text('Welcome!', style: Theme.of(context).textTheme.displayMedium),
+        ),
+    );
+  }
+
+}
 
 class SignUpForm extends StatefulWidget {
   const SignUpForm();
@@ -49,6 +63,10 @@ class _SignUpFormState extends State<SignUpForm> {
   final _usernameTextController = TextEditingController();
 
   double _formProgress = 0;
+
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -94,7 +112,7 @@ class _SignUpFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed: null,
+            onPressed: _showWelcomeScreen,
             child: const Text('Sign up'),
           ),
         ],
